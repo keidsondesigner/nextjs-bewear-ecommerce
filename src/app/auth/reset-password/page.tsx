ca764a58b33874722bcc7f6bed5d3f699b817fa6
Keidson Roby <keidsondesigner@gmail.com>
2025-08-13 21:48:23 -0400

    feat(reset-password): Criado página Resetar senha

---
+++ src/app/auth/reset-password/page.tsx
@@ -0,0 +1,11 @@
+import ResetPasswordForm from "@/components/emails/reset-password-form";
+
+export default function ResetPasswordPage() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen w-full p-5">
+        <ResetPasswordForm />
+      </div>
+    </>
+  );
+}
