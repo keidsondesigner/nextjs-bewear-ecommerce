afcd2488d7f91e58c8ef51dde4dea4468c94508c
Keidson Roby <keidsondesigner@gmail.com>
2025-08-18 11:03:00 -0400

    feat: Criado Footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="max-w-[1280px] mx-auto px-5 space-y-6 mb-12">
-      <div className="bg-accent w-full gap-1 p-8">
+      <div className="bg-accent w-full gap-1 p-8 rounded-lg">
         <p className="text-xs font-medium">&copy; 2024 BeWear. Todos os direitos reservados.</p>
         <p className="text-muted-foreground text-xs font-medium">
           Todos os direitos reservados.
