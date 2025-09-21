8177721ef3f417448bcb59514d7b52650ffc4f7e
Keidson Roby <keidsondesigner@gmail.com>
2025-09-21 19:32:38 -0400

    feat(cart): Adicionado componente AddToCartButton para integrar a adição de produtos ao carrinho com ReactQuery

---
+++ src/app/product-variant/[slug]/components/add-to-cart-button.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addCartProduct } from "@/actions/add-cart-product";
+import { Loader2 } from "lucide-react";
+
+interface AddToCartButtonProps {
+  productVariantId: string;
+  quantity: number;
+}
+
+// Recebemos o id da variante do produto e a quantidade
+const AddToCartButton = ({ productVariantId, quantity }: AddToCartButtonProps) => {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["addCartProduct", productVariantId, quantity],
+    mutationFn: () => addCartProduct({ // 1. chamamos a action addCartProduct para adicionar o produto no carrinho.
+      productVariantId,
+      quantity,
+    }),
+    onSuccess: () => {
+      // 2. o cache da query ["cart"] do carrinho é invalidado, após adicionar o produto no carrinho.
+      // 3. isso faz com que a query seja refetchada[recarregar automaticamente], e o carrinho seja atualizado.
+      // 4. a query ["cart"] é uma query que busca o carrinho do usuário. queryKey: ["cart"], queryFn: () =>  getCart(),
+      queryClient.invalidateQueries({ queryKey: ["cart"] });
+    },
+  });
+
+  return (
+    <Button
+      className="rounded-full"
+      size="lg"
+      variant="outline"
+      disabled={isPending}
+      onClick={() => mutate()}
+    >
+      {isPending && <Loader2 className="animate-spin" />}
+      Adicionar ao carrinho
+    </Button>
+  );
+};
+
+export default AddToCartButton;
