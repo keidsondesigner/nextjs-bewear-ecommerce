b81f35d2c18fd4de6bf2d86fc3de15a2b15ec536
Keidson Roby <keidsondesigner@gmail.com>
2025-09-21 19:31:51 -0400

    feat(cart): Integrecao chamada de dados do carrinho com o ReactQuery e exiba os itens no carrinho

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,8 +1,17 @@
 import { ShoppingBasketIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
+import { useQuery } from "@tanstack/react-query";
+import { getCart } from "@/actions/get-cart";
+import Image from "next/image";
+import { formatCentsToBRL } from "@/app/helpers/format-money-brl";
 
 const Cart = () => {
+  const { data: cart, isPending: cartIsLoading } = useQuery({
+    queryKey: ["cart"],
+    queryFn: () =>  getCart(),
+  });
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -16,7 +25,21 @@ const Cart = () => {
             <SheetTitle>Carrinho</SheetTitle>
           </SheetHeader>
           <div>
-            <p>Cart</p>
+            { cartIsLoading && <p>Carregando...</p>}
+
+            {cart?.cartItem.map((item) => (
+              <div key={item.id}>
+                <Image
+                  src={item.productVariant.imageUrl}
+                  alt={item.productVariant.name}
+                  width={100}
+                  height={100}
+                />
+                <p>{item.productVariant.name}</p>
+                <p>{item.quantity}</p>
+                <p>{formatCentsToBRL(item.productVariant.priceInCents)}</p>
+              </div>
+            ))}
           </div>
       </SheetContent>
     </Sheet>
