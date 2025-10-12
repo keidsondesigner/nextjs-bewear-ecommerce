36834c279f5fbeef58db24875143e62285f52d42
Keidson Roby <keidsondesigner@gmail.com>
2025-10-12 11:59:51 -0400

    feat(component-addresses): Implementar o componente Endereços para exibir e gerenciar endereços de entrega com hooks useShippingAddresses, useCreateShippingAddressMutation

---
+++ src/app/cart/identification/components/addresses.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useState } from "react";
+import AddAddressForm from "./add-address-form";
+import { useShippingAddresses } from "@/hooks/queries/use-shipping-addresses";
+import { useCreateShippingAddressMutation } from "@/hooks/mutations/use-create-shipping-address";
+import { CreateShippingAddressSchema } from "@/actions/create-shipping-address/schema";
+
+const Addresses = () => {
+  const [selectedAddresses, setselectedAddresses] = useState<string | null>(null);
+  const { data: shippingAddresses, isLoading } = useShippingAddresses();
+  const addShippingAddressMutation = useCreateShippingAddressMutation();
+
+  const handleAddAddress = async (address: CreateShippingAddressSchema) => {
+    try {
+      const newAddress = await addShippingAddressMutation.mutateAsync(address);
+      setselectedAddresses(newAddress.id);
+    } catch (error) {
+      console.error("Error adding address:", error);
+    }
+  };
+  if (isLoading) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Endereços</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-sm text-muted-foreground">Carregando endereços...</p>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Endereços</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="mb-8">
+          <RadioGroup value={selectedAddresses} onValueChange={setselectedAddresses}>
+            {shippingAddresses?.map((address) => (
+              <Card key={address.id}>
+                <div className="flex gap-4 px-5 items-center">
+                  <RadioGroupItem value={address.id} id={address.id} />
+                  <Label htmlFor={address.id} className="cursor-pointer">
+                    <div className="text-sm">
+                      <p className="font-medium">
+                        {address.recipientName}: {address.street}, {address.number}
+                        {address.complement ? `, ${address.complement}` : ""}
+                      </p>
+                      <p className="text-muted-foreground">
+                        {address.neighborhood}, {address.city} - {address.state}
+                      </p>
+                    </div>
+                  </Label>
+                </div>
+              </Card>
+            ))}
+
+            <Card>
+              <div className="flex gap-4 px-5 items-center">
+                <RadioGroupItem value="add_new_address" id="add_new_address" />
+                <Label htmlFor="add_new_address" className="cursor-pointer">Adicionar novo</Label>
+              </div>
+            </Card>
+          </RadioGroup>
+        </div>
+
+        {selectedAddresses === "add_new_address" && (
+          <AddAddressForm onSuccess={handleAddAddress} />
+        )}
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Addresses;
