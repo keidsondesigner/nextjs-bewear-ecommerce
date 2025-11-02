99bd9dbb78b4f536a5c385dc35c48b677dbb28b1
Keidson Roby <keidsondesigner@gmail.com>
2025-11-02 18:45:27 -0400

    feat(ResetPassword): wrap ResetPasswordForm in Suspense for improved loading state

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import ResetPasswordForm from "@/components/emails/reset-password-form";
 
 export default function ResetPasswordPage() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen w-full p-5">
-        <ResetPasswordForm />
+        <Suspense fallback={<div>Carregando...</div>}>
+          <ResetPasswordForm />
+        </Suspense>
       </div>
     </>
   );
